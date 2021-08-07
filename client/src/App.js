6555e5f2e39fc4c11cae87e4ce1a996064098ace
Joey Davidson <j.davidson18@gmail.com>
2021-08-07 09:25:53 -0600

    bootstrap linked in to app baseline

--- client/src/App.js
+++ client/src/App.js
@@ -3,19 +3,20 @@ import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import './App.css';
 import About from './pages/about'
 import Discover from './pages/discover'
-import Home from './pages/home'
 import Search from './pages/search'
+import NavBar from './components/NavBar/NavBar';
 
 function App() {
   return (
 
     <Router>
+      <NavBar/>
       <Switch>
         <Route exact path={'/'}>
-          <Home/>
+          <About/>
         </Route>
         <Route exact path={'/home'}>
-          <Home/>
+          <About/>
         </Route>
         <Route exact path={'/about'}>
           <About/>
--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+<link
+  rel="stylesheet"
+  href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
+  integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
+  crossorigin="anonymous"
+/>
 
 
 ReactDOM.render(
